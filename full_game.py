3aaaa3aaacb646158cbad5cd816731cdda93b6ea
rclavey <96779746+rclavey@users.noreply.github.com>
2022-01-15 20:31:56 -0500

    Update full_game.py

--- full_game.py
+++ full_game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 # set size of game
 pygame.init()
 WIDTH, HEIGHT = 400, 400
@@ -23,9 +24,7 @@ FONT = pygame.font.Font("freesansbold.ttf", 25)
 j = 0
 i = 0
 positions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-record = []
 sel = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-winner = None
 # opening menu
 def menu_screen():
     WINDOW.fill((WHITE))
@@ -90,121 +89,65 @@ def set_window():
     pygame.display.update()
 
 
-# check for a win condition
-def check_win():
-    global positions
-    global j
-    global winner
-    # check rows for a win
-    if positions[0] == positions[1] and positions[1] == positions[2] and positions[0] == 1:
-        print("Game Over Row One")
-        print("Player One Wins")
-        winner = 1
-        return True
-    if positions[3] == positions[4] and positions[4] == positions[5] and positions[3] == 1:
-        print("Game Over Row Two")
-        print("Player One Wins")
-        winner = 1
-        return True
-    if positions[6] == positions[7] and positions[7] == positions[8] and positions[6] == 1:
-        print("Game Over Row Three")
-        print("Player One Wins")
-        winner = 1
-        return True
-    # check coloumns for a win
-    if positions[0] == positions[3] and positions[3] == positions[6] and positions[0] == 1:
-        print("Game Over Col One")
-        print("Player One Wins")
-        winner = 1
-        return True
-    if positions[1] == positions[4] and positions[4] == positions[7] and positions[1] == 1:
-        print("Game Over Col Two")
-        print("Player One Wins")
-        winner = 1
-        return True
-    if positions[2] == positions[5] and positions[5] == positions[8] and positions[2] == 1:
-        print("Game Over Col Two")
-        print("Player One Wins")
-        winner = 1
-        return True
-    # check diagnols for a win
+# check wins for x
+def check_x(move, positions):
+    # get the col the previous move was in
+    c = move % 3
+    r = move // 3
+    # check that col to see if they are all x
+    for i in range(len(positions)):
+        if i % 3 == c and positions[c] == 1 and positions[c + 3] == 1 and positions[c + 6] == 1:
+            return True
+    for i in range(len(positions)):
+        if i // 3 == r and positions[r * 3] == 1 and positions[(r * 3) + 1] == 1 and positions[(r * 3) + 2] == 1:
+            return True
     if positions[0] == positions[4] and positions[4] == positions[8] and positions[0] == 1:
-        print("Game Over Diag One")
-        print("Player One Wins")
-        winner = 1
         return True
     if positions[2] == positions[4] and positions[4] == positions[6] and positions[2] == 1:
-        print("Game Over Diag Two")
-        print("Player One Wins")
-        winner = 1
         return True
+    return False
 
-    # check rows for win
-    if positions[0] == positions[1] and positions[1] == positions[2] and positions[0] == 2:
-        print("Game Over Row One")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    if positions[3] == positions[4] and positions[4] == positions[5] and positions[3] == 2:
-        print("Game Over Row Two")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    if positions[6] == positions[7] and positions[7] == positions[8] and positions[6] == 2:
-        print("Game Over Row Three")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    # check coloumns for a win
-    if positions[0] == positions[3] and positions[3] == positions[6] and positions[0] == 2:
-        print("Game Over Col One")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    if positions[1] == positions[4] and positions[4] == positions[7] and positions[1] == 2:
-        print("Game Over Col Two")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    if positions[2] == positions[5] and positions[5] == positions[8] and positions[2] == 2:
-        print("Game Over Col Three")
-        print("Player Two Wins")
-        winner = 2
-        return True
-    # check diagnols for a win
+
+# check wins for y
+def check_y(move, positions):
+        # get the col the previous move was in
+    c = move % 3
+    r = move // 3
+    # check that col to see if they are all x
+    for i in range(len(positions)):
+        if i % 3 == c and positions[c] == 2 and positions[c + 3] == 2 and positions[c + 6] == 2:
+            return True
+    for i in range(len(positions)):
+        if i // 3 == r and positions[r * 3] == 2 and positions[(r * 3) + 1] == 2 and positions[(r * 3) + 2] == 2:
+            return True
     if positions[0] == positions[4] and positions[4] == positions[8] and positions[0] == 2:
-        print("Game Over Diag One")
-        print("Player Two Wins")
-        winner = 2
         return True
     if positions[2] == positions[4] and positions[4] == positions[6] and positions[2] == 2:
-        print("Game Over Diag Two")
-        print("Player Two Wins")
-        winner = 2
         return True
-    else:
-        return False
+    return False
+
+# check for a win condition
+def check_win(move, positions):
+    return check_x(move, positions) or check_y(move, positions)
 
 
 # reset game when over
 def reset_game():
+    time.sleep(3)
     global positions
     global j
     global i
-    global record
     global sel
     j = 0
     i = 0
     positions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    record = []
     sel = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
 # check for a tie
-def check_tie():
-    global positions
-    if (positions[0] and positions[1] and positions[2] and positions[3] and positions[4] and positions[5] and positions[6] and positions[7] and positions[8] != 0) and not check_win():
-        print("Tie Game")
+def check_tie(move, positions):
+    if (positions[0] and positions[1] and positions[2] and positions[3] and positions[4] and positions[5] and positions[6] and positions[7] and positions[8] != 0) and not check_win(move, positions):
+        #print("Tie Game")
         return True
     else:
         return False
@@ -214,9 +157,7 @@ def check_tie():
 def easy():
     # get global variables
     global positions
-    global record
     global multi
-    global winner
     # set variables
     clock = pygame.time.Clock()
     run = True
@@ -233,39 +174,22 @@ def easy():
                 if pygame.mouse.get_pressed()[0]:
                     player = True
                     if player:
-                        pos = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
-                        if pos == (0,0):
-                            pos = 0
-                        if pos == (1,0):
-                            pos = 1
-                        if pos == (2,0):
-                            pos = 2
-                        if pos == (0,1):
-                            pos = 3
-                        if pos == (1,1):
-                            pos = 4
-                        if pos == (2,1):
-                            pos = 5
-                        if pos == (0,2):
-                            pos = 6
-                        if pos == (1,2):
-                            pos = 7
-                        if pos == (2,2):
-                            pos = 8
+                        col, row = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
+                        # set move equal to the position of board clicked on, row is multiplied by three because moving one row down moves it three in list
+                        move = col + 3 * row
                         # if player one moves set corresponding position to 1 as long as move isnt already taken, for player to set to 2
-                        if not record.count(pos) > 0:
-                            positions[pos] = 1
-                            record.append(pos)
-                            sel.pop(sel.index(pos))
+                        if positions[move] == 0:
+                            positions[move] = 1
+                            sel.pop(sel.index(move))
                             player = False
                         else:
                             print("Not a valid move")
-                        if check_win() or check_tie():
+                        if check_win(move, positions) or check_tie(move, positions):
                             run = False
-                    if not player and not check_win() and not check_tie():
+                    if not player:
                         easy_move()
                         player = True
-                        if check_win() or check_tie():
+                        if check_win(move, positions) or check_tie(move, positions):
                             run = False
         set_window()
     reset_game()
@@ -277,24 +201,20 @@ def easy():
 def easy_move():
     global positions
     global sel
-    global record
     move = random.choice(sel)
     index = sel.index(move)
-    if not record.count(move) > 0:
+    if positions[move] == 0:
         positions[move] = 2
-        record.append(move)
         sel.pop(index)
     else:
         easy_move()
 
 
 # generate hard move using min_max()
-def hard_move():
-    global positions
-    global record
+def hard_move(positions):
     # set best score absurdly low and set best move to a place holder
     best_score = -1000
-    best_move = 0
+    best_move = -1
     # iterate through all positions
     for k in range(len(positions)):
         # only calculate for positions that are empty
@@ -302,7 +222,8 @@ def hard_move():
             # set position equal to computer move to evaluate strength of position
             positions[k] == 2
             # call min_max function in order to get score of current iteration
-            score = min_max(positions, True)
+            score: int = min_max(positions, k, True)
+            print("the score is:", score, "for position: ", k)
             # set position back to unoccupied so board is not changed prematurely
             positions[k] = 0
             # not reaching here, means k_score is not more than best score
@@ -315,25 +236,23 @@ def hard_move():
     print()
     print("Thinking Finished") 
     print() 
-    return int(best_move)
+    return best_move
     
 
 # recursive min_max function returns score of moves 
-def min_max(board, maxing):
-    global winner
-    global positions
+def min_max(positions: list, move: int, maxing: bool):
     # base cases
     # if winner is x, which is the player, return -1
-    if winner == 1:
+    if check_x(move, positions):
         return -1
     # if winner is o, which is the computer, return 1
-    if winner == 2:
+    elif check_y(move, positions):
         return 1
     # if game is a tie, return 0
-    if check_tie():
+    elif check_tie(move, positions):
         return 0
     # if trying to maximize score to find computers best move do the following:
-    if maxing:
+    elif maxing:
         # set the best score to an absurdly low number
         best_score = -1000
         # iterate through all the positions in the game
@@ -343,20 +262,16 @@ def min_max(board, maxing):
                 # set each of these positions to the computers move
                 positions[k] = 2
                 # recursively call this function with the new board, and set maxing to false so the next iteration will be minimizing
-                score = min_max(positions, False)
+                score: int = min_max(positions, k, False)
                 # set position back to 0 as to not disturb the board before a move is played
-                winner = None
                 positions[k] = 0
                 # if the score for k is better than the current best score, replace it
-                if score is not None:
-                    if score > best_score:
-                        best_score = score
-        return best_score
+                if score > best_score:
+                    best_score = score
     # if trying to minimize score to find opponents best move do the following:
     else:
         # set the best score to an absurdly high number to be replaced by lower numbers
         best_score = 1000
-        score = int
         # iterate through all positions in the game
         for k in range(len(positions)):
             # only do calculations for positions that are open
@@ -364,25 +279,21 @@ def min_max(board, maxing):
                 # if position k is open, mark it as x
                 positions[k] = 1
                 # recursively call this function with the new board and set maxing to true so the next iteration will be maximizing
-                score = min_max(positions, True)
+                score: int = min_max(positions, k, True)
                 # set position of k back to 0 so that it does not change the board prior to making a move
-                winner = None
                 positions[k] = 0
                 # if the score of k is lower than the best score, replace the best score with k
-                if score is not None:
-                    if score < best_score:
-                        best_score = score
-        # after iterating through all the possible game states, return the best score
-        return int(best_score)
+                if score < best_score:
+                    best_score = score
+    # after iterating through all the possible game states, return the best score
+    return best_score
 
 
 # hard mode computer with perfect moves via min_max
 def hard():
     # get global variables
     global positions
-    global record
     global multi
-    global winner
     # set variables
     clock = pygame.time.Clock()
     run = True
@@ -399,51 +310,22 @@ def hard():
                 if pygame.mouse.get_pressed()[0]:
                     player = True
                     if player:
-                        pos = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
-                        if pos == (0,0):
-                            pos = 0
-                        if pos == (1,0):
-                            pos = 1
-                        if pos == (2,0):
-                            pos = 2
-                        if pos == (0,1):
-                            pos = 3
-                        if pos == (1,1):
-                            pos = 4
-                        if pos == (2,1):
-                            pos = 5
-                        if pos == (0,2):
-                            pos = 6
-                        if pos == (1,2):
-                            pos = 7
-                        if pos == (2,2):
-                            pos = 8
-                        # if player one moves set corresponding position to 1 as long as move isnt already taken, for player to set to 2
-                        if not check_win() and not check_tie(): 
-                            if not record.count(pos) > 0:
-                                positions[pos] = 1
-                                record.append(pos)
-                                print("code got to here")
-                                comp = hard_move()
-                                positions[comp] = 2
-                                record.append(comp)
-                                print(positions)
-                                print("this is the computer move: ", comp)
-                                print("code got all the way down here")
-                            else:
-                                print("Not a valid move")
-                        elif check_win or check_tie():
-                            run = False
-                        # if not player and not check_win() and not check_tie():
-                        #     print("here is the move im thinking of: ", hard_move())
-                        #     pos = hard_move()
-                        #     positions[pos] = 2
-                        #     record.append(pos)
-                        #     #sel.pop(sel.index(pos))
-                        #     player = True
-                        #     if check_win() or check_tie():
-                        #         run = False
-                        # print(positions)
+                        col, row = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
+                        # set move equal to the position of board clicked on, row is multiplied by three because moving one row down moves it three in list
+                        move = col + 3 * row
+                        # if player one moves set corresponding position to 1 as long as move isnt already taken, for player to set to 
+                        if positions[move] == 0:
+                            positions[move] = 1
+                            if check_win(move, positions) or check_tie(move, positions):
+                                run = False
+                            comp = hard_move(positions)
+                            positions[comp] = 2
+                            if check_win(comp, positions) or check_tie(comp, positions):
+                                run = False
+                            print(positions)
+                            print("this is the computer move: ", comp)
+                        else:
+                            print("Not a valid move")
         set_window()
     reset_game()
     multi = False
@@ -507,13 +389,10 @@ def tic_tac_toe_multi():
     global positions
     global j
     global i
-    global record
     global multi
-    global winner
     # set variables
     clock = pygame.time.Clock()
     run = True
-    
     # set infinite while loop
     while run:
         clock.tick(FPS)
@@ -527,37 +406,20 @@ def tic_tac_toe_multi():
                 if pygame.mouse.get_pressed()[0]:
                     i += 1
                     j = i % 2
-                    pos = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
-                    if pos == (0,0):
-                        pos = 0
-                    if pos == (1,0):
-                        pos = 1
-                    if pos == (2,0):
-                        pos = 2
-                    if pos == (0,1):
-                        pos = 3
-                    if pos == (1,1):
-                        pos = 4
-                    if pos == (2,1):
-                        pos = 5
-                    if pos == (0,2):
-                        pos = 6
-                    if pos == (1,2):
-                        pos = 7
-                    if pos == (2,2):
-                        pos = 8
+                    col, row = (pygame.mouse.get_pos()[0] // 133), (pygame.mouse.get_pos()[1] // 133)
+                    # set move equal to the position of board clicked on, row is multiplied by three because moving one row down moves it three in list
+                    move = col + 3 * row
                     # if player one moves set corresponding position to 1 as long as move isnt already taken, for player to set to 2
-                    if not record.count(pos) > 0:
+                    if positions[move] == 0:
                         if j:
-                            positions[pos] = 1
-                            record.append(pos)
+                            positions[move] = 1
                         if not j:
-                            positions[pos] = 2
-                            record.append(pos)
+                            positions[move] = 2
                     else:
                         i -= 1
                         print("Not a valid move")
-                    if check_win() or check_tie():
+                    #if check_win() or check_tie():
+                    if check_win(move, positions) or check_tie(move, positions):
                         run = False
         set_window()
     reset_game()
